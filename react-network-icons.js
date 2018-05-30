a2a23148685730bdcbb3075014ca143b99064111
Reece Jones <reece.c.jones@gmail.com>
2018-05-30 12:06:34 +0100

    Release v2.9.2

---
+++ react-network-icons.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.__esModule = true;
+exports.NetworkIcon = undefined;
+
+var _socialIcon = require('./social-icon');
+
+var _socialIcon2 = _interopRequireDefault(_socialIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.NetworkIcon = _socialIcon2.default;
